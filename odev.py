25ca883338ed6c21a990060db1ebbcb86d9dc453
Yildiz Mercan <e.yldzmercan@gmail.com>
2023-01-27 18:22:51 +0300

    birinci ödev

---
+++ odev.py
@@ -0,0 +1,46 @@
+print("""
+
+	=_=Hesap Makinesi=_=
+	(1)Toplama
+	(2)Çıkarma
+	(3)Çaprma
+	(4)Bölme
+	=_==_==_==_==_=_=
+
+""")
+
+veri=input("işlem:")
+
+if veri=="1":
+	x=input("ilk sayı:")
+	xfloat(x)
+	y=input("ikinci sayı:")
+	y=float(y)
+	print(sonuç:",x+y")
+
+elif veri=="2":
+	x=input("ilk sayı:")
+	xfloat(x)
+	y=input("ikinci sayı:")
+	y=float(y)
+	print(sonuç:",x-y")
+
+elif veri=="3":
+	x=input("ilk sayı:")
+	xfloat(x)
+	y=input("ikinci sayı:")
+	y=float(y)
+	print(sonuç:",x*y")
+
+elif veri=="4":
+	x=input("ilk sayı:")
+	xfloat(x)
+	y=input("ikinci sayı:")
+	y=float(y)
+	print(sonuç:",x/y")
+	
+	else:
+		print("yanlış seçim:(program kapanıyor...)"
+			quit()
+	
+	
